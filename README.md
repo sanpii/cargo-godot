860a1c6a85fc70739fb49cd365f82ff7da3d6df8
Sanpi <sanpi@homecomputing.fr>
2024-05-19 14:01:15 +0200

    Adds doc

---
+++ README.md
@@ -0,0 +1,29 @@
+# cargo-godot
+
+[![Crates.io](https://img.shields.io/crates/v/cargo-godot)](https://crates.io/crates/cargo-godot)
+[![Github actions](https://github.com/sanpii/cargo-godot/workflows/.github/workflows/ci.yml/badge.svg)](https://github.com/sanpii/cargo-godot/actions?query=workflow%3A.github%2Fworkflows%2Fci.yml)
+[![pipeline status](https://gitlab.com/sanpi/cargo-godot/badges/main/pipeline.svg)](https://gitlab.com/sanpi/cargo-godot/-/commits/main)
+
+Cargo helper to improve godot rust developement experience.
+
+## Install
+
+```
+cargo install cargo-godot
+```
+
+## Use
+
+In your rust project, you should add metadata in the Cargo.toml to specify the
+godot project path:
+
+```toml
+[package.metadata.godot]
+project = "../godot"
+```
+
+Then, you can directly run/export/debug your project directly via cargo:
+
+```
+cargo godot run
+```
--- src/opt.rs
+++ src/opt.rs
@@ -7,36 +7,47 @@ pub enum Opt {
 
 #[derive(Debug, clap::Subcommand)]
 pub enum Command {
+    /// Launch game via LLDB
     Debug(DebugOpt),
+    /// Launch project in Godot
     Editor(EditorOpt),
+    /// Export game
     Export(ExportOpt),
+    /// Run game
     Run(RunOpt),
 }
 
 #[derive(Debug, clap::Parser)]
 pub struct DebugOpt {
+    /// Path to Cargo.toml
     #[arg(long, default_value = "./Cargo.toml")]
     pub manifest_path: std::path::PathBuf,
 }
 
 #[derive(Debug, clap::Parser)]
 pub struct EditorOpt {
+    /// Path to Cargo.toml
     #[arg(long, default_value = "./Cargo.toml")]
     pub manifest_path: std::path::PathBuf,
 }
 
 #[derive(Debug, clap::Parser)]
 pub struct ExportOpt {
+    /// Path to Cargo.toml
     #[arg(long, default_value = "./Cargo.toml")]
     pub manifest_path: std::path::PathBuf,
+    /// Build and export in release mode
     #[arg(long, default_value_t = false)]
     pub release: bool,
+    /// Preset name
     pub preset: String,
+    /// Output path. ./build/ by default
     pub path: Option<std::path::PathBuf>,
 }
 
 #[derive(Debug, clap::Parser)]
 pub struct RunOpt {
+    /// Path to Cargo.toml
     #[arg(long, default_value = "./Cargo.toml")]
     pub manifest_path: std::path::PathBuf,
 }
